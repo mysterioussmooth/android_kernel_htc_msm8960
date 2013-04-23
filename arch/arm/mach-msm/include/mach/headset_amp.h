278d7d359a8454b12268bd62e5641b708dcd4b04
Flemmard <flemmard@gmail.com>
2013-04-23 09:58:15 +0200

    Add dummy headset_amp_event for other devices (not M7) (else build will fail)

--- arch/arm/mach-msm/include/mach/headset_amp.h
+++ arch/arm/mach-msm/include/mach/headset_amp.h
@@ -1,6 +1,10 @@
 #ifndef HEADSET_AMP_H
 #define HEADSET_AMP_H
 
+#ifdef CONFIG_MACH_M7_UL
 extern void headset_amp_event(unsigned int on);
+#else
+void headset_amp_event(unsigned int on) { };
+#endif
 
 #endif
